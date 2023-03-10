7462a20b6dd1725292d8a3d570a0374c37ab777d
jenil2209 <jenilparmar1999@gmail.com>
2023-03-10 11:38:09 +0530

    Conversion Logic implemented

--- app/src/main/java/com/jenil/conversionapp/MainActivity.kt
+++ app/src/main/java/com/jenil/conversionapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.jenil.conversionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.jenil.conversionapp.databinding.ActivityMainBinding
 
@@ -9,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     val units = arrayOf("km", "mi", "m", "ft", "yd", "cm", "mm")
+    var answer = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +26,74 @@ class MainActivity : AppCompatActivity() {
             if (binding.etAmount.text.isEmpty()) {
                 binding.etAmount.error = "Please enter amount"
             } else {
+                val result = answer * binding.etAmount.text.toString().toDouble()
+                binding.tvAnswer.text = result.toString()
+            }
+        }
 
+        binding.spSource.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                when (p2) {
+                    0 -> {
+                        val answerArray =
+                            arrayOf(1.0, 0.621371, 1000.0, 3280.84, 1093.61, 100000.0, 1000000.0)
+                        setDestinationSpinner(answerArray)
+                    }
+                    1 -> {
+                        val answerArray =
+                            arrayOf(1.60934, 1.0, 1609.34, 5280.0, 1760.0, 160934.0, 1609344.0)
+                        setDestinationSpinner(answerArray)
+                    }
+                    2 -> {
+                        val answerArray =
+                            arrayOf(0.001, 0.000621371, 1.0, 3.28084, 1.09361, 100.0, 1000.0)
+                        setDestinationSpinner(answerArray)
+                    }
+                    3 -> {
+                        val answerArray =
+                            arrayOf(0.0003048, 0.000189394, 0.3048, 1.0, 0.33333, 30.48, 304.8)
+                        setDestinationSpinner(answerArray)
+                    }
+                    4 -> {
+                        val answerArray =
+                            arrayOf(0.0009144, 0.000568182, 0.9144, 3.0, 1.0, 91.44, 914.4)
+                        setDestinationSpinner(answerArray)
+                    }
+                    5 -> {
+                        val answerArray =
+                            arrayOf(0.00001, 0.0000062137, 0.01, 0.010936133, 0.0328, 1.0, 10.0)
+                        setDestinationSpinner(answerArray)
+                    }
+                    6 -> {
+                        val answerArray =
+                            arrayOf(
+                                1.0 / 1000000.0,
+                                1.0 / 1609344.0,
+                                1.0 / 1000.0,
+                                1.0 / 0.001094,
+                                1.0 / 305.0,
+                                1.0 / 10.0,
+                                1.0
+                            )
+                        setDestinationSpinner(answerArray)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setDestinationSpinner(answerArray: Array<Double>) {
+        binding.spDest.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                answer = answerArray[p2]
             }
         }
     }
